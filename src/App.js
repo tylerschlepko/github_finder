ee65bd79a819fa413b4867b7701ffc76d393d1e4
tylerschlepko <97323708+tylerschlepko@users.noreply.github.com>
2023-02-15 13:17:37 -0600

    implemented a reducer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Footer from "./components/layout/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
-import { GithubProvider } from "./context/GithubContext";
+import { GithubProvider } from "./context/github/GithubContext";
 
 function App() {
   return (
--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {useEffect, useContext} from 'react'
 import Spinner from '../layout/Spinner'
 import UserItem from './UserItem'
-import GithubContext from '../../context/GithubContext'
+import GithubContext from '../../context/github/GithubContext'
 
 function UserResults() {
     const {users, loading, fetchUsers} = useContext(GithubContext)
