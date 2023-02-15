3a53942f7c1c658e7a2331a4c6c070d5ea2e7968
tylerschlepko <97323708+tylerschlepko@users.noreply.github.com>
2023-02-15 14:38:38 -0600

    started single user page

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -8,23 +8,68 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 export const GithubProvider = ({children}) => {
     const initialState = {
         users: [],
-        loading: true
+        user: {},
+        loading: false,
     }
 
     const [state, dispatch] = useReducer(GithubReducer, initialState)
 
-    const fetchUsers = async () => {
-        const response = await fetch(`${GITHUB_URL}/users`, {
+    //Get search results
+    const searchUsers = async (text) => {
+        setLoading()
+
+        const params = new URLSearchParams({
+            q: text
+        })
+
+        const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
             headers: {
                 Authorization: `token ${GITHUB_TOKEN}`
             }
         })
 
-        const data = await response.json()
+        const {items} = await response.json()
         
         dispatch({
             type: 'GET_USERS',
-            payload: data
+            payload: items,
+        })
+    }
+
+    //Get a single Usere
+    const getUser = async (login) => {
+        setLoading()
+
+        const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`
+            }
+        })
+
+        if(response.status === 404){
+            window.location = '/notfound'
+        } else {
+            const data = await response.json()
+            
+            dispatch({
+                type: 'GET_USER',
+                payload: data,
+            })
+        }
+
+    }
+
+    //Clears Users
+    const clearUsers = () => {
+        dispatch({
+            type: 'CLEAR_USERS'
+        })
+    }
+
+    //Set Loading 
+    const setLoading = () => {
+        dispatch({
+            type: 'SET_LOADING'
         })
     }
 
@@ -32,7 +77,11 @@ export const GithubProvider = ({children}) => {
     value={{
         users : state.users, 
         loading : state.loading, 
-        fetchUsers}}>
+        user: state.user,
+        searchUsers,
+        clearUsers,
+        getUser,
+        }}>
         {children}
     </GithubContext.Provider>
 }
--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -6,6 +6,22 @@ const GithubReducer = (state, action) =>{
                 users: action.payload,
                 loading: false,
             }
+        case 'GET_USER':
+            return{
+                ...state,
+                user: action.payload,
+                loading: false,
+            }
+        case 'SET_LOADING':
+            return {
+            ...state,
+            loading: true
+            }
+        case "CLEAR_USERS":
+            return{
+                ...state,
+                users: []
+            }
         default: 
             return state
 
